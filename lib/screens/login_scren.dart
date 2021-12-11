987cc85cd0ff4676a3319e1ece225eb4d57ffa8d
User <faysaltanvir991@gmail.com>
2021-12-11 15:53:51 +0600

    fast page

--- lib/screens/login_scren.dart
+++ lib/screens/login_scren.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -7,6 +9,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final double height=MediaQuery.of(context).size.height;
+  final  double width=MediaQuery.of(context).size.width;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,13 +26,70 @@ class _LoginScreenState extends State<LoginScreen> {
           Container(
             child: Column(
               children: [
-                Text.rich(TextSpan(
-                  text: "HEARD\t",
-                  style: TextStyle(letterSpacing: 5,fontSize: 30,color: Colors.blue)
-                ))
+                Center(
+                  child: Text.rich(
+
+                    TextSpan(
+
+                    text: "WORKOUT",
+                      style: GoogleFonts.lato(
+                        textStyle: TextStyle(color: Colors.white, letterSpacing: .5, fontSize:26
+
+                        ),
+
+
+                      ),
+
+                    children: [
+                      TextSpan(
+                        text: "  EVERYDAY",
+                          style: GoogleFonts.lato(
+                      textStyle: TextStyle(color: Colors.blue, letterSpacing: .5, fontSize:26
+                      ),
+                          ),
+
+                      ),
+                    ]
+
+
+                  ),
+
+
+                  ),
+                ),
+                Spacer(),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    children: [
+                      Text("wellcome"
+                      , style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,color: Colors.white),
+                      ),
+                      SizedBox(height: 17,),
+                      Text(
+                          "Train and live the new exprience of \n exercsing at home",
+                        style: TextStyle(color: Colors.white),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30,),
+                TextButton(
+
+                    onPressed: (){},
+                    child: Container(
+                      height: 50,
+                       width: width*0.07,
+                      decoration: BoxDecoration(
+
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+
+                    ))
               ],
             ),
-          )
+          ),
+          Container()
         ],
       ),
     );
